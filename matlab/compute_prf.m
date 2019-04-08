7fa84a29cb963f956e119bda758e3694c05046d2
Margriet <margrietlinota@hotmail.com>
2019-04-08 09:58:12 +0200

    use first volume, plus FIX: double find

--- matlab/compute_prf.m
+++ matlab/compute_prf.m
@@ -1,7 +1,6 @@
 function compute_prf(nifti, output_dir, threshold)
 %COMPUTE_PRF
 
-
 hdr = niftiinfo(nifti{1});
 n_volumes = hdr.ImageSize(3);
 TR = hdr.PixelDimensions(4);
@@ -14,15 +13,14 @@ for i = 1:length(nifti)
     nii{i} = niftiread(nifti{i});
 end
 
-nii_cat = cat(4, nii{:});
-mean_nii = mean(nii_cat, 4);
-vxs = find(mean_nii > threshold);
+first_nii = nii{1}(:, :,:, 1);
+vxs = find(first_nii > threshold);
 
 n_dim = size(nii{1});
 n_vox = prod(n_dim(1:3));
 fprintf('Selecting %d out of %d voxels (%.2f%%)\n', length(vxs), n_vox, length(vxs)/ n_vox *100)
 
-results = analyzePRF({images, images}, nii, TR, struct('seedmode',[0 1],'display','off', 'wantglmdenoise', 0, 'vxs', find(vxs)));
+results = analyzePRF({images, images}, nii, TR, struct('seedmode',[0 1],'display','off', 'wantglmdenoise', 0, 'vxs', vxs));
 
 
 hdr.ImageSize = hdr.ImageSize(1:3);
